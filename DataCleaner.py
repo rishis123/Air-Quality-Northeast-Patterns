1e614b803380d54b6e79996de49dd74278a72d49
Harsh Shah <71080727+SamShah30112004@users.noreply.github.com>
2023-07-30 15:43:28 +0530

    Updated for final changes in DataCleaner.py

--- DataCleaner.py
+++ DataCleaner.py
@@ -1,30 +1,59 @@
-import pandas as pd #This program cleans the data from the original data-extracter, including taking arithmetic means (if numerous datapoints in a single year, single gas, and single state). 
-
-df = pd.read_excel('extracted_data_2000.xlsx')
-
-df.head()
+import pandas as pd
 
 arr = [9, 23, 25, 33, 34, 36, 42, 44, 50]
 
 arr1 = ['Carbon monoxide', 'Ozone', 'Sulfur dioxide', 'Nitrogen dioxide (NO2)']
 
-arr2 = []
+for k in range(0,10):
+    df = pd.read_excel('extracted_data_200' + str(k) + '.xlsx')
 
-arr3 = []
+    df.head()
 
-arr4 = []
+    arr2 = []
 
-for i in arr:
-    for j in arr1:
-        new_df = df[(df["State Code"] == i) & (df['Parameter Name'] == j)]
-        arr2.append(new_df["Arithmetic Mean"].mean())
-        arr3.append(j)
-        arr4.append(i)
+    arr3 = []
 
-data = pd.DataFrame({
-    'State Names': arr4,
-    'Gases': arr3,
-    'Mean Value': arr2
-})
+    arr4 = []
 
-data.to_excel('Output.xlsx')
+    for i in arr:
+        for j in arr1:
+            new_df = df[(df["State Code"] == i) & (df['Parameter Name'] == j)]
+            arr2.append(new_df["Arithmetic Mean"].mean())
+            arr3.append(j)
+            arr4.append(i)
+
+    data = pd.DataFrame({
+        'State Names': arr4,
+        'Gases': arr3,
+        'Mean Value': arr2
+    })
+
+    data.to_excel('extracted_data_200' + str(k) + '.xlsx')
+    print(k)
+
+for k in range(10,23):
+    df = pd.read_excel('extracted_data_20' + str(k) + '.xlsx')
+
+    df.head()
+
+    arr2 = []
+
+    arr3 = []
+
+    arr4 = []
+
+    for i in arr:
+        for j in arr1:
+            new_df = df[(df["State Code"] == i) & (df['Parameter Name'] == j)]
+            arr2.append(new_df["Arithmetic Mean"].mean())
+            arr3.append(j)
+            arr4.append(i)
+
+    data = pd.DataFrame({
+        'State Names': arr4,
+        'Gases': arr3,
+        'Mean Value': arr2
+    })
+
+    data.to_excel('extracted_data_20' + str(k) + '.xlsx')
+    print(k)
