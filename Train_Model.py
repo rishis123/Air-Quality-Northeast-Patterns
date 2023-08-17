dca99f634fb0d919dc3d29eef2f1ed10964ae9f2
Harsh Shah <71080727+SamShah30112004@users.noreply.github.com>
2023-08-17 18:00:28 +0530

    Updated to use Random Forest Regression model

--- Train_Model.py
+++ Train_Model.py
@@ -1,15 +1,12 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error, accuracy_score
 from sklearn import preprocessing
 from sklearn.metrics import r2_score
 
 # importing data
-df = pd.read_excel('/Users/rishishah/PycharmProjects/ClimateSi/venv/CarbonMonoxideFile.xlsx')
+df = pd.read_excel('Data/CarbonMonoxideFile.xlsx')
 df = df.dropna()
 df['Mean Value'] = pd.to_numeric(df['Mean Value'], errors='coerce')
 
@@ -21,24 +18,21 @@ IQR = Q3 - Q1
 # Filter outliers from the "Mean Value" column
 # df = df[~((df['Mean Value'] < (Q1 - 1.5 * IQR)) | (df['Mean Value'] > (Q3 + 1.5 * IQR)))]
 
-X = df.drop(['Gases', 'Unnamed: 0', 'Mean Value'], axis= 1)
+X = df.drop(['Gases', 'Unnamed: 0', 'Mean Value'], axis=1)
 y = df['Mean Value']
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=101)
+    X, y, test_size=0.2)
 
-print(X.head())
+model = RandomForestRegressor()
 
-model = LinearRegression()
+model.fit(X_train, y_train)  # Train model on training data (80% of dataset)
 
-model.fit(X_train,y_train) # Train model on training data (80% of dataset)
-
-predictions = model.predict(X_test) # Run model on testing data (20% of dataset)
+# Run model on testing data (20% of dataset)
+predictions = model.predict(X_test)
 
 r_squared = r2_score(y_test, predictions)
 
-print(
-  'mean_squared_error : ', mean_squared_error(y_test, predictions))
-print(
-  'mean_absolute_error : ', mean_absolute_error(y_test, predictions))
+print('mean_squared_error : ', mean_squared_error(y_test, predictions))
+print('mean_absolute_error : ', mean_absolute_error(y_test, predictions))
 print(r_squared)
